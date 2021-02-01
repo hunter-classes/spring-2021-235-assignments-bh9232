bc398a63bf9f31b01b49b729172bd4ba1350c1ed
bh9232 <bhong9232@gmail.com>
2021-02-01 13:06:52 -0500

    fib-iter_done

--- recursion/basics.cpp
+++ recursion/basics.cpp
@@ -1,21 +1,5 @@
 #include <iostream>
-/*
-6! = 6*5!
-  5! = 5*4!
-  4! = 4*6
-  3! = 3*2
-  2! = 2*1
-  1! = 1 *1
-  0! = 1
-  fact(n) = {  1 if n = 0
-               n * (n-1)! otherwise
-Things to look for in a recursive solution:
-1. Base case <-- when you just know the answer without recurring
-2. reduction <-- the recursive call that will eventually get to the base case
-Things to consider
-1. Try small examples by hand
-2. look for self similarity - smaller simpler problems 
-*/
+
 
 int fact(int n){
   if (n == 0){
@@ -24,12 +8,6 @@ int fact(int n){
     return n * fact(n-1);
   }
 }
-
-/*
- *
- 1 1 2 3 5 8 13 21 34
- the next fib number is the sum of the previous two fib numbers
-*/
 
 int fib(int n){
 
@@ -63,14 +41,8 @@ int fib_iter(int n){
 
 int main()
 {
-  int i;
-
-  // // factorial
-  // for (i=0; i < 10 ; i++){
-  //   std::cout << i << "! = " << fact(i) << "\n";
-  // }
-
   // // fib sequence (recursion)
+  // int i;
   // for (i=0; i < 10 ; i++){
   //   std::cout << fib(i) << ", ";
   // }
